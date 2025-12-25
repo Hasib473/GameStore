2f18cb3e66c13ad8f7bde27a60ef343f55d5365a
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-12-25 23:29:02 +0600

    commit test first

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,16 +3,19 @@ import ImageSlider from '../Component/ImageSlider';
 
 const Home = () => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div className='w-full mx-auto p-4'>
 
-            <div className='text-center mb-4'>
+            <div className='text-center mb-10 mt-6'>
                 <h1 className="text-3xl font-bold mt-3">Welcome to <span className='text-purple-400'>GameStore</span></h1>
                 <p className="mt-2 text-gray-600">
                     Explore the best indie games and support your favorite developers.
                 </p>
             </div>
             {/* Banner / Hero Slider */}
+            <div className='w-full mx-auto mb-10 mt-8'>
             <ImageSlider />
+
+            </div>
 
 
         </div>
